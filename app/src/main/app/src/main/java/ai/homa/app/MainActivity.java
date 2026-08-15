64ef4bfdca815a5998e81a994de595c51fad1085
شیرکو <228393493+shrkoo@users.noreply.github.com>
2026-08-15 11:41:05 +0330

    Update MainActivity.java

--- app/src/main/app/src/main/java/ai/homa/app/MainActivity.java
+++ app/src/main/app/src/main/java/ai/homa/app/MainActivity.java
@@ -1,24 +1,7 @@
 package ai.homa.app;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.widget.TextView;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BridgeActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        TextView textView = new TextView(this);
-
-        textView.setText("Homa AI");
-        textView.setTextSize(30);
-        textView.setTextColor(Color.BLACK);
-        textView.setGravity(Gravity.CENTER);
-
-        setContentView(textView);
-    }
 }
